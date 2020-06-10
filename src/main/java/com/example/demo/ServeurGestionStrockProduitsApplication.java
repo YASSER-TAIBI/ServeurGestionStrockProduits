77b24b47b1fb340b5a3c2060f85fa1dfaeac4d83
unknown <YASSER-TAIBI>
2020-06-10 08:36:45 +0100

    first commit

---
+++ src/main/java/com/example/demo/ServeurGestionStrockProduitsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStrockProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStrockProduitsApplication.class, args);
+	}
+
+}
